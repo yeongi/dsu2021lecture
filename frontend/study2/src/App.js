8deac0e455e1ef579e931b34d09a750bf620b97d
yeongi <cbg5255@gmail.com>
2021-11-21 17:26:27 +0900

    useeffect

--- frontend/study2/src/App.js
+++ frontend/study2/src/App.js
@@ -1,29 +1,33 @@
-import {useState} from 'react';
+import react, {useEffect, useState} from 'react';
 import './App.css';
 import Child from './Child';
 
 function App() {
 
-  const odj = {name : "이름" , value : "장의영"};
-
-  const {id, value} = odj;
-
   const [name, setName] = useState("장의영");
 
-  // const renderMarkUp = () => {
-  //   return (
-  //     <div>
-  //       <h1>뾰로로로룡</h1>
-  //       <Child name = {name}/>
-  //       <span>{name}</span>
-  //     </div>
-  //   )
-  // }
+  //state를 배열로 관리하기 위함
+  const [list, setList] = useState([]);
+
+
+  useEffect(()=>{
+    if(name === "장의영"){
+      alert(name);
+    }
+    
+  },[name]);
+
+  const onAppendName = () => {
+    console.log(":되니??");
+    setList(list.concat(name));
+  }
 
   return (
     <div>
-      <button onClick={()=>{setName("없어짐")}}> 클릭 </button>
-      <Child name = {name} onchangeName = {(value)=>{setName(value)}} />
+      <input value={name} onChange={e =>setName(e.target.value)}/>
+      <button onClick={onAppendName}> 추가 </button>
+      <br/>
+      {list.map(item => <div>{item}</div>)}
     </div>
     
   );
